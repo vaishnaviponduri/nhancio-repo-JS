941d89b1909ece9d504fd8f2bf5940b69400a7eb
vedantshah46 <vedantshah022@gmail.com>
2024-10-12 17:47:11 +0530

    NavBar Commit One

--- nhancio/src/App.js
+++ nhancio/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import HomePage from './MyComponent/HomePage/HomePage'
+import Navbar from './MyComponent/Navbar/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <HomePage/>
     </div>
   )
