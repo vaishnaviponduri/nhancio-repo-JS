a3f4c934e5b167c92d2ce47b82bb4b293a738c67
Yatri-patel <yatripatel02@gmail.com>
2024-10-23 22:09:51 +0530

    AboutUs Completed

--- nhancio/src/App.js
+++ nhancio/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ChooseUs from "./MyComponent/ChooseUs/ChooseUs";
 import HomePage from "./MyComponent/HomePage/HomePage";
 import Navbar from "./MyComponent/Navbar/Navbar";
 import Footer from "./MyComponent/Footer/Footer";
@@ -14,7 +13,6 @@ const App = () => {
     <>
       <Navbar />
       <HomePage />
-      <ChooseUs />
       <ContactUs />
       <Services/>
       <Client/>
